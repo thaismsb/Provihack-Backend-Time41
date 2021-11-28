c6505b5ef4224e4ca52079f303ffa2a7e0cd8bc5
Thais Barbosa <thaismsbrasil@hotmail.com>
2021-11-27 21:55:01 -0300

    conexao com o banco de dados

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'locais_acessiveis',
-    password: 'Know-how',
+    user: 'evimrlluwjsvjh',
+    host: 'ec2-18-213-133-45.compute-1.amazonaws.com',
+    database: 'd84lthiacls0be',
+    password: '5249b361acca3db0202dc32cf0c5159c0aa7561d33f7a5aff272699f87773f0d',
     port: 5432,
     ssl: { rejectUnauthorized: false }
 });
